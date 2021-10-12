750144d378eb8187cd17e4da7e0588c8c6fab8c0
Mikael Gråborg <mikael.graborg@iteam.se>
2021-10-11 19:21:08 -0500

    add character creator

---
+++ character-creator/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import CharacterEditor from './components/CharacterEditor';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <div className="background-divider"></div>
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
