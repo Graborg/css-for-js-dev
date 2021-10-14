118768ba1959438d21630b1d159a554bb9f71634
Mikael Gråborg <mikael.graborg@iteam.se>
2021-10-14 11:20:02 -0500

    mini-components: add progressbar

--- mini-component-library/src/components/ProgressBar/ProgressBar.js
+++ mini-component-library/src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,36 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let height;
+  if (size === "large") {
+    height = "16px";
+  } else if (size === "medium") {
+    height = "12px";
+  } else {
+    height = "8px";
+  }
+  return (
+      <Wrapper size={size}> 
+        <Bar role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow={value} value={value} height={height}/> 
+      </Wrapper>
+  );
 };
+
+
+let Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
+  border-radius: 8px;
+  padding: ${p => p.size === "large" && "4px"};
+`
+
+let Bar = styled.div`
+  --border-rad: ${p => p.value > 99 && (p.value - 99) * 4}px;
+  height: ${p => p.height};
+  background-color: ${COLORS.primary};
+  border-radius: 4px var(--border-rad) var(--border-rad) 4px;
+
+  width: ${p => p.value}%;
+`
 
 export default ProgressBar;
