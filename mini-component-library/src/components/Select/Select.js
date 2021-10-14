e305546f9c196612a2166b341de69a61b0426446
Mikael Gråborg <mikael.graborg@iteam.se>
2021-10-14 11:20:17 -0500

    mini-components: add dropdown (after peeking at solution)

--- mini-component-library/src/components/Select/Select.js
+++ mini-component-library/src/components/Select/Select.js
@@ -9,10 +9,62 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper> 
+      <HiddenSelect value={value} onChange={onChange}>
+        {children}
+      </HiddenSelect>
+      <VisualSelect>
+        <Value> { displayedValue }</Value>
+        <ChevronDown strokeWidth="2" size="22" id="chevron-down" />
+      </VisualSelect>
+    </Wrapper>
   );
 };
+
+let ChevronDown = styled(Icon)`
+  position: absolute;
+  right: 10px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+`
+let Value = styled.p`
+  width: fit-content;
+  font-size: 16px;
+  line-height:19px;
+`
+
+let HiddenSelect = styled.select`
+  border: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
+let VisualSelect = styled.div`
+  position: relative; 
+  padding: 12px;
+  padding-right: 52px;
+  pointer-events: none;
+  color: ${COLORS.gray700}; 
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+
+  ${HiddenSelect}:hover + & {
+    color: black;
+  }
+
+  ${HiddenSelect}:focus + & {
+    outline: 2px solid #4374CB;
+    border-radius: 3px;
+  }
+`
+
+let Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`
 
 export default Select;
