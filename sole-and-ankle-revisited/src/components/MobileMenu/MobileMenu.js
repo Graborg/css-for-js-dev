f9dc18dea6307a21c77397ae06ea792129cba537
Mikael Gråborg <mikael.graborg@iteam.se>
2022-01-01 22:42:41 -0600

    add mobile menu styles

--- sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
+++ sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, COLORS } from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
@@ -15,23 +15,80 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
+    <Wrapper>
+      <Close onClick={onDismiss}/>
+      <Nav>
         <a href="/sale">Sale</a>
         <a href="/new">New&nbsp;Releases</a>
         <a href="/men">Men</a>
         <a href="/women">Women</a>
         <a href="/kids">Kids</a>
         <a href="/collections">Collections</a>
-      </nav>
-      <footer>
+      </Nav>
+      <Footer>
         <a href="/terms">Terms and Conditions</a>
         <a href="/privacy">Privacy Policy</a>
         <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+      </Footer>
+    </Wrapper>
   );
 };
+const Nav = styled.nav`
+  & a {
+    text-decoration: none;
+    color: black;
+    text-transform: uppercase;
+    line-height: 21px;
+    font-weight: 500;
+  }
+  & a:first-child {
+    color: ${COLORS.secondary} ;
+  }
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  margin-top: auto;
+`
+
+const Footer = styled.footer`
+  & a {
+    text-decoration: none;
+    color: ${COLORS.gray[700]};
+  }
+  display: flex;
+  flex-direction: column;
+  margin-top: auto;
+`
+
+const Close = styled.button`
+  background: white;
+  height: 25px;
+  width: 25px;
+  border: 0;
+  position: absolute;
+  top: -2px;
+  right: 22px;
+  &:after {
+    color: black;
+    font-size: 50px; 
+    content: "×";
+  }
+`
+
+const Wrapper = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 80vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  background: white;
+  z-index: 2;
+  height: 100vh;
+  padding-left: 32px;
+  padding-bottom: 32px;
+`
 
 export default MobileMenu;
