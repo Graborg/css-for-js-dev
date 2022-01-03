d9513ee26a5b94c9a71d5458a134913d117cdf50
Mikael Gråborg <mikael.graborg@iteam.se>
2022-01-02 22:55:24 -0600

    use @reach for accessibility in mobile menu

--- sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
+++ sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
@@ -10,30 +10,28 @@ import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <Wrapper>
-      <CloseButton onClick={onDismiss}>
-        <Icon aria-hidden id="close" size={20} />
-        <VisuallyHidden>Close</VisuallyHidden>
-      </CloseButton>
-      <Nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </Nav>
-      <Footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </Footer>
-    </Wrapper>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Content aria-label="mobilemenu">
+        <CloseButton onClick={onDismiss}>
+          <Icon aria-hidden id="close" size={20} />
+          <VisuallyHidden>Close</VisuallyHidden>
+        </CloseButton>
+        <Nav>
+          <a href="/sale">Sale</a>
+          <a href="/new">New&nbsp;Releases</a>
+          <a href="/men">Men</a>
+          <a href="/women">Women</a>
+          <a href="/kids">Kids</a>
+          <a href="/collections">Collections</a>
+        </Nav>
+        <Footer>
+          <a href="/terms">Terms and Conditions</a>
+          <a href="/privacy">Privacy Policy</a>
+          <a href="/contact">Contact Us</a>
+        </Footer>
+      </Content>
+    </Overlay>
   );
 };
 const Nav = styled.nav`
@@ -67,25 +65,30 @@ const CloseButton = styled.button`
   background: white;
   border: none;
   position: absolute;
-  top: 32px;
+  top: 28px;
   right: 22px;
 `
 
-const Wrapper = styled.div`
-  box-shadow: -120px -1px 0 1px rgba(96, 100, 108, 0.8);
+const Content = styled(DialogContent)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 80%;
+  padding-left: 32px;
+  padding-bottom: 32px;
+  background: white;
+`
+
+const Overlay = styled(DialogOverlay)`
   position: fixed;
   right: 0;
   top: 0;
-  width: 80vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  background: white;
-  z-index: 2;
-  height: 100vh;
-  padding-left: 32px;
-  padding-bottom: 32px;
+  width: 100%;
+  height: 100%;
+  background: rgba(96, 100, 108, 0.8);
 `
 
 export default MobileMenu;
