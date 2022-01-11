356a4d398dc339de85269653847c150839ce0227
Mikael Gråborg <mikael.graborg@iteam.se>
2022-01-10 18:18:24 -0600

    add unsprinkle and change image formats & optimize font

---
+++ unsprinkle/src/components/PhotoGridItem/PhotoGridItem.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const PhotoGridItem = ({ id, src, alt, tags }) => {
+  return (
+    <article>
+      <Anchor href={`/photos/${id}`}>
+        <picture>
+          <source 
+            type="image/avif"
+            srcset={` 
+              ${src.replace(".jpg", ".avif")} 1x,
+              ${src.replace(".jpg", "@2x.avif")} 2x,
+              ${src.replace(".jpg", "@3x.avif")} 3x
+            `
+            }
+          />
+          <Image 
+            src={src}
+            srcset={` 
+              ${src} 1x,
+              ${src.replace(".jpg", "@2x.jpg")} 2x,
+              ${src.replace(".jpg", "@3x.jpg")} 3x
+            `
+            }
+          />
+        </picture>
+      </Anchor>
+      <Tags>
+        {tags.map((tag) => (
+          <Tag key={tag}>{tag}</Tag>
+        ))}
+      </Tags>
+    </article>
+  );
+};
+
+const Anchor = styled.a`
+  text-decoration: none;
+  color: inherit;
+  outline-offset: 4px;
+`;
+
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 300px;
+  border-radius: 2px;
+  margin-bottom: 8px;
+`;
+
+const Tags = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+const Tag = styled.li`
+  padding: 4px 8px;
+  background: var(--color-gray-300);
+  font-size: 0.875rem;
+  font-weight: 475;
+  color: var(--color-gray-800);
+`;
+
+export default PhotoGridItem;
