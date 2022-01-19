4cd936ee169a7291abfdaa80cebfff383b60b83c
Mikael Gråborg <mikael.graborg@iteam.se>
2022-01-19 11:31:19 -0600

    New grid times: add teacher's fixes for Footer, see below
    Same solution as teacher. But reorganized and add whitespace
    for readability and reduced complexity

--- new-grid-times/src/components/Footer/Footer.js
+++ new-grid-times/src/components/Footer/Footer.js
@@ -145,11 +145,13 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+
   @media ${QUERIES.tabletAndUp}{
     flex-direction: row;
     justify-content: center;
     gap: 48px;
   }
+
   @media ${QUERIES.laptopAndUp}{
     justify-content: flex-end;
   }
@@ -179,19 +181,16 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+
   @media ${QUERIES.tabletAndUp} {
     display: grid;
     grid-template-columns:
       repeat(auto-fit, minmax(200px, 1fr));
-    justify-content: center;
+    text-align: left;
   }
 `;
 
-const NavWrapper = styled.nav`
-  @media ${QUERIES.tabletAndUp} {
-    text-align: left
-  }
-`
+const NavWrapper = styled.nav``
 
 const MainNavHeading = styled.h2`
   font-size: 1.125rem;
@@ -217,6 +216,7 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
   @media ${QUERIES.laptopAndUp} {
     align-items: flex-start;
   }
